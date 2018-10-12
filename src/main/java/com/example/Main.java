753fba292f8b561ad08e79f468af6a7f3981f627
DESKTOP-TQIF9RM\doble <ayala.santiago.leonardo@gmail.com>
2018-10-12 16:43:49 -0500

    a
    a

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -40,7 +40,6 @@ import java.sql.Connection;
 import java.sql.DriverManager; 
 import com.example.UserJDBCTemplate;
 
-package com.example;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
