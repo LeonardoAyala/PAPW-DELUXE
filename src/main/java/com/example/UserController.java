72dd3d966d37e2eaf151e896fd9045c7e9c2e265
DESKTOP-TQIF9RM\doble <ayala.santiago.leonardo@gmail.com>
2018-10-12 16:54:46 -0500

    a
    a

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -5,6 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+import java.net.URI; 
+import java.net.URISyntaxException; 
+import java.sql.Connection; 
+import java.sql.DriverManager; 
+import com.example.UserJDBCTemplate;
+
 
 @Controller
 public class UserController {
