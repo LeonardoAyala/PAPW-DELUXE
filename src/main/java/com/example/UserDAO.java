a7f4e197341429476065d4afd1b5fb57a27f025f
DESKTOP-TQIF9RM\doble <ayala.santiago.leonardo@gmail.com>
2018-10-12 11:55:15 -0500

    a
    a

--- src/main/java/com/example/UserDAO.java
+++ src/main/java/com/example/UserDAO.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 public interface UserDAO {    
     public void setDataSource(Connection connection);        
     public void create(String name, Integer age);        
-    public Student getUser(Integer id);        
+    public User getUser(Integer id);        
     public List<User> listUsers();        
     public void delete(Integer id);        
     public void update(Integer id, Integer age); 
