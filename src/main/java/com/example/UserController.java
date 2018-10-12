12346032e298341b7d845b7c9d04fa4eddfe3e8f
DESKTOP-TQIF9RM\doble <ayala.santiago.leonardo@gmail.com>
2018-10-12 16:59:42 -0500

    a
    a

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -1,4 +1,16 @@
 package com.example;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.jdbc.core.JdbcTemplate; 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
