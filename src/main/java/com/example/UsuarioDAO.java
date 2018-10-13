5f2af9d19be5c3dea9678d7549d20082a9cedcb2
DESKTOP-TQIF9RM\doble <ayala.santiago.leonardo@gmail.com>
2018-10-13 10:00:50 -0500

    a
    a

---
+++ src/main/java/com/example/UsuarioDAO.java
@@ -0,0 +1,15 @@
+package com.example; 
+import java.util.List; 
+import javax.sql.DataSource; 
+import java.sql.Connection;
+ 
+public interface UsuarioDAO {    
+    public void setDataSource(Connection connection);        
+    public void create(String nombreUsuario, String apellido, String correo, String username,
+        String contrasena, String telefono, String direccion);        
+    public User getUsuario(Integer ID_Usuario);        
+    public List<Usuario> listUsuario();        
+    public void delete(Integer ID_Usuario);        
+    public void update(Integer ID_Usuario, String nombreUsuario, String apellido, String correo, String username,
+        String contrasena, String telefono, String direccion);
+} 
---
+++ src/main/java/com/example/UsuarioJDBCTemplate.java
@@ -0,0 +1,67 @@
+package com.example; 
+import java.util.List; 
+import java.sql.Connection;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+
+public class UsuarioJDBCTemplate implements UsuarioDAO {    
+
+private JdbcTemplate jdbcTemplateObject; 
+ 
+public void setDataSource(Connection connection) {   
+    this.jdbcTemplateObject =      
+        new JdbcTemplate(      
+            new SingleConnectionDataSource(connection, false)     
+    ); 
+} 
+ 
+public void create(String nombreUsuario, 
+    String apellido, String correo, String username,
+    String contrasena, String telefono, String direccion) {   
+    String SQL =      
+        "insert into User (nombreUsuario, apellido, 
+        correo, username, contrasena, telefono, 
+        direccion) values (?, ?, ?, ?, ?, ?, ?)";   
+    jdbcTemplateObject.update(SQL, nombreUsuario, apellido, correo, username, 
+    contrasena, telefono, direccion);   
+    System.out.println(
+             "Registro creado = " + nombreUsuario
+             );   
+    return; 
+}
+
+public User getUsuario(Integer ID_Usuario) {
+    String SQL = "select * from Usuario where ID_Usuario = ?";   
+    Usuario usuario = jdbcTemplateObject.queryForObject(SQL,       
+        new Object[]{ID_Usuario}, new UserMapper());   
+    return usuario; 
+} 
+ 
+public List<Usuario> listUsers() {   
+    String SQL = "select * from Usuario";   
+    List <Usuario> usuarios =      
+    jdbcTemplateObject.query(SQL, new UserMapper());   
+    return usuarios; 
+}
+
+public void delete(Integer ID_Usuario) {
+String SQL = "delete from Usuario where ID_Usuario = ?";
+jdbcTemplateObject.update(SQL, ID_Usuario);
+System.out.println("Borrado ID_Usuario = " + ID_Usuario );
+return;
+}
+
+public void update(Integer ID_Usuario ,String nombreUsuario, 
+    String apellido, String correo, String username,
+    String contrasena, String telefono, String direccion) {
+    String SQL = "update Usuario set nombreUsuario = ?, apellido = ?, correo = ?, username = ?,
+    contrasena = ?, telefono = ?, direccion = ? where ID_Usuario = ?";
+    jdbcTemplateObject.update(SQL, nombreUsuario, apellido, correo, username,
+         contrasena, telefono, direccion, ID_Usuario);
+    System.out.println("Actualizado ID = " + ID_Usuario );
+    return;
+}
+
+
+} 
