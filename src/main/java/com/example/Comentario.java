b292a8e08eafcf54eeedb403444093a9510ea884
DESKTOP-TQIF9RM\doble <ayala.santiago.leonardo@gmail.com>
2018-10-13 13:23:00 -0500

    a
    a

---
+++ src/main/java/com/example/Comentario.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Comentario {
+    private Integer ID_Comentario;   
+    private Integer ID_Usuario;   
+    private Integer ID_Articulo;   
+    private Date fechaComentario;   
+    private String comentario;      
+
+    public Integer getId() {return ID_Comentario;}  
+
+    public void setIdUsuario(Integer ID_Usuario) {this.ID_Usuario = ID_Usuario;}   
+    public String getIdUsuario() {return ID_Usuario;}     
+
+    public void setIdArticulo(Integer ID_Articulo) {this.ID_Articulo = ID_Articulo;}   
+    public String getIdarticulo() {return ID_Articulo;} 
+
+    public void setFecha(Date fechaComentario) {this.fechaComentario = fechaComentario;}   
+    public String getFecha() {return fechaComentario;}     
+
+    public void setComentario(String comentario) {this.comentario = comentario;}   
+    public String getComentario() {return comentario;}      
+
+}
