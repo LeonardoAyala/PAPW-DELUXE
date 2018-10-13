4798b27f72e932418ac97c8e2316b6d458483efb
DESKTOP-TQIF9RM\doble <ayala.santiago.leonardo@gmail.com>
2018-10-13 17:02:07 -0500

    a
    a

--- src/main/java/com/example/Articulo_CategoriaMapper.java
+++ src/main/java/com/example/Articulo_CategoriaMapper.java
@@ -7,8 +7,8 @@ import org.springframework.jdbc.core.RowMapper;
 public class Articulo_CategoriaMapper implements RowMapper<Articulo_Categoria> {
     public Articulo_Categoria mapRow(ResultSet rs, int rowNum) 
         throws SQLException {       
-        Articulo articulo = new Articulo();
-        articulo.setIdCategoria(rs.getInt("ID_Categoria"));       
-        articulo.setIdArticulo(rs.getInt("ID_Articulo"));         
+        Articulo_Categoria articuloCategoria = new Articulo_Categoria();
+        articuloCategoria.setIdCategoria(rs.getInt("ID_Categoria"));       
+        articuloCategoria.setIdArticulo(rs.getInt("ID_Articulo"));         
     } 
 } 
--- src/main/java/com/example/Articulo_PagoMapper.java
+++ src/main/java/com/example/Articulo_PagoMapper.java
@@ -7,8 +7,8 @@ import org.springframework.jdbc.core.RowMapper;
 public class Articulo_PagoMapper implements RowMapper<Articulo_Pago> {
     public Articulo_Pago mapRow(ResultSet rs, int rowNum) 
         throws SQLException {       
-        Articulo articulo = new Articulo();
-        articulo.setIdPago(rs.getInt("ID_Pago"));       
-        articulo.setIdArticulo(rs.getInt("ID_Articulo"));         
+        Articulo_Pago articuloPago = new Articulo_Pago();
+        articuloPago.setIdPago(rs.getInt("ID_Pago"));       
+        articuloPago.setIdArticulo(rs.getInt("ID_Articulo"));         
     } 
 } 
