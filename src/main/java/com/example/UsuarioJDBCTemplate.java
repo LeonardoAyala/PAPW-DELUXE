fdbccdfb9b2b2994f14f17199e3af8853bfe574f
DESKTOP-TQIF9RM\doble <ayala.santiago.leonardo@gmail.com>
2018-11-17 22:35:29 -0600

    a
    a

--- src/main/java/com/example/UsuarioJDBCTemplate.java
+++ src/main/java/com/example/UsuarioJDBCTemplate.java
@@ -34,7 +34,7 @@ public void create(Usuario usuario) {
         "direccion, imagen_avatar, imagen_portada) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";   
     jdbcTemplateObject.update(SQL, usuario.getNombreUsuario(), usuario.getApellido(),
     usuario.getCorreo(), usuario.getUsername(), usuario.getContrasena(), 
-    usuario.getTelefono(), (byte)0, (byte)0
+    usuario.getTelefono(), usuario.getDireccion(), (byte)0, (byte)0
     /*usuario.getImagen_avatar(), usuario.getImagen_portada() */);   
     System.out.println("Registro creado = " + usuario.getNombreUsuario());   
     return; 
