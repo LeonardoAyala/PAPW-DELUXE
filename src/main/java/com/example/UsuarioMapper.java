690e1775dabc0ff4e34f17339891f1b95da6ca98
DESKTOP-TQIF9RM\doble <ayala.santiago.leonardo@gmail.com>
2018-11-17 21:43:39 -0600

    a
    a

--- src/main/java/com/example/UsuarioMapper.java
+++ src/main/java/com/example/UsuarioMapper.java
@@ -15,8 +15,8 @@ public class UsuarioMapper implements RowMapper<Usuario> {
         usuario.setContrasena(rs.getString("contrasena"));       
         usuario.setTelefono(rs.getString("telefono"));              
         usuario.setDireccion(rs.getString("direccion"));   
-        usuario.setImagen_portada(rs.setBytes("imagen_portada"));    
-        usuario.setImagen_avatar(rs.setBytes("imagen_avatar"));    
+        usuario.setImagen_portada(rs.getBytes("imagen_portada"));    
+        usuario.setImagen_avatar(rs.getBytes("imagen_avatar"));    
         return usuario;    
     } 
 } 
