c3bc0a3222cc235adb46b679105fbc737ed28c78
DESKTOP-TQIF9RM\doble <ayala.santiago.leonardo@gmail.com>
2018-12-05 22:58:56 -0600

    a
    a

--- src/main/java/com/example/ArticuloController.java
+++ src/main/java/com/example/ArticuloController.java
@@ -309,7 +309,7 @@ public class ArticuloController {
                 //return "redirect:/item";
             }
 */
-            return "redirect:/Catalog";
+            return "Catalog";
     }
 
 
